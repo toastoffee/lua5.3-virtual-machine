b3fd674c44a9decf315effb3ebe37861fd75e766
toastoffee <931391442@qq.com>
2024-07-13 15:38:31 +0800

    add instruction decode

---
+++ src/instruction_decode.hpp
@@ -0,0 +1,45 @@
+/**
+  ******************************************************************************
+  * @file           : instruction_decode.hpp
+  * @author         : toastoffee
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2024/7/13
+  ******************************************************************************
+  */
+
+
+
+#ifndef LUA5_3_DECOMPILER_INSTRUCTION_DECODE_HPP
+#define LUA5_3_DECOMPILER_INSTRUCTION_DECODE_HPP
+
+#include "instruction_types.hpp"
+#include "chunk_types.hpp"
+
+const int MAX_ARG_Bx = (1 << 18) - 1;
+const int MAX_ARG_sBx = MAX_ARG_Bx >> 1;
+
+uint32 GetOpcode(Instruction i) {
+    return i & 0x3f;
+}
+
+void ABC(Instruction i, int& a, int& b, int& c) {
+    a = (int)((i >> 6) & 0xff);
+    b = (int)((i >> 14) & 0x1ff);
+    c = (int)((i >> 23) & 0x1ff);
+    return;
+}
+
+void ABx(Instruction i, int& a, int& bx) {
+    a =  (int)((i >> 7) & 0xff);
+    bx = (int)(i >> 15);
+    return;
+}
+
+void AsBx(Instruction i, int& a, int& sbx) {
+    ABx(i, a, sbx);
+    sbx -= MAX_ARG_sBx;
+    return;
+}
+
+#endif //LUA5_3_DECOMPILER_INSTRUCTION_DECODE_HPP
