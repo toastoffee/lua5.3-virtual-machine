91db95cebd2ac5fd0c28832d23e85505a07b1394
toastoffee <931391442@qq.com>
2024-07-13 23:50:38 +0800

    complete instruction decode

--- src/instruction_decode.hpp
+++ src/instruction_decode.hpp
@@ -31,14 +31,19 @@ void ABC(Instruction i, int& a, int& b, int& c) {
 }
 
 void ABx(Instruction i, int& a, int& bx) {
-    a =  (int)((i >> 7) & 0xff);
-    bx = (int)(i >> 15);
+    a =  (int)((i >> 6) & 0xff);
+    bx = (int)(i >> 14);
     return;
 }
 
 void AsBx(Instruction i, int& a, int& sbx) {
     ABx(i, a, sbx);
     sbx -= MAX_ARG_sBx;
+    return;
+}
+
+void Ax(Instruction i, int& a) {
+    a = (int)(i >> 6);
     return;
 }
 
