acffa1b7c64b19d254c7624df9db046cca65c46d
toastoffee <931391442@qq.com>
2024-07-14 10:18:11 +0800

    add lua consts

---
+++ src/lua_consts.hpp
@@ -0,0 +1,28 @@
+/**
+  ******************************************************************************
+  * @file           : lua_consts.hpp
+  * @author         : toastoffee
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2024/7/14
+  ******************************************************************************
+  */
+
+
+
+#ifndef LUA5_3_DECOMPILER_LUA_CONSTS_HPP
+#define LUA5_3_DECOMPILER_LUA_CONSTS_HPP
+
+const int LUA_TNONE = -1,
+          LUA_TNIL = 0,
+          LUA_TBOOLEAN = 1,
+          LUA_TLIGHTUSERDATA = 2,
+          LUA_TNUMBER = 3,
+          LUA_TSTRING = 4,
+          LUA_TTABLE = 5,
+          LUA_TFUNCTION = 6,
+          LUA_TUSERDATA = 7,
+          LUA_TTHREAD = 8;
+
+
+#endif //LUA5_3_DECOMPILER_LUA_CONSTS_HPP
