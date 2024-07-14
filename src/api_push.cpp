65e1274dab65cdea1642d9dee87171246c6a7483
toastoffee <931391442@qq.com>
2024-07-14 21:38:14 +0800

    add api_push

---
+++ src/api_push.cpp
@@ -0,0 +1,31 @@
+/**
+  ******************************************************************************
+  * @file           : api_push.cpp
+  * @author         : toastoffee
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2024/7/14
+  ******************************************************************************
+  */
+
+#include "lua_state.hpp"
+
+void LuaState::PushNil() {
+    _stack->Push(nil);
+}
+
+void LuaState::PushBoolean(bool b) {
+    _stack->Push(LuaValue(b));
+}
+
+void LuaState::PushInteger(int64 n) {
+    _stack->Push(LuaValue(n));
+}
+
+void LuaState::PushNumber(float64 n) {
+    _stack->Push(LuaValue(n));
+}
+
+void LuaState::PushString(std::string s) {
+    _stack->Push(LuaValue(s));
+}
