28ef64cf2cc6d43afd8f993ca7b7825fb054f344
toastoffee <931391442@qq.com>
2024-07-16 00:03:57 +0800

    finish lua state opes

---
+++ src/state/api_misc.cpp
@@ -0,0 +1,41 @@
+/**
+  ******************************************************************************
+  * @file           : api_misc.cpp
+  * @author         : toastoffee
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2024/7/15
+  ******************************************************************************
+  */
+
+#include "lua_state.hpp"
+
+void LuaState::Len(int idx) {
+    LuaValue val = _stack->Get(idx);
+
+    if(val.GetType() == typeid(std::string).name()) {
+        auto newVal = LuaValue((int64)val.GetVal<std::string>().length());
+        _stack->Push(newVal);
+    } else {
+        assert(false && "length error!");
+    }
+}
+
+void LuaState::Concat(int n) {
+    if (n == 0) {
+        _stack->Push(LuaValue(""));
+    }
+    else if(n >= 2) {
+        for (int i = 0; i < n; ++i) {
+            if(IsString(-1) && IsString(-2)) {
+                std::string s2 = ToString(-1);
+                std::string s1 = ToString(-2);
+                _stack->Pop();
+                _stack->Pop();
+                _stack->Push(LuaValue(s1+s2));
+                continue;
+            }
+        }
+        assert(false && "concatenation error!");
+    }
+}
