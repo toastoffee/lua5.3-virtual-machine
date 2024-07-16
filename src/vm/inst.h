24be4f80b956d94a58b6a0a85f34c371c0925827
toastoffee <931391442@qq.com>
2024-07-16 16:01:36 +0800

    complete move and jmp

---
+++ src/vm/inst.h
@@ -0,0 +1,32 @@
+/**
+  ******************************************************************************
+  * @file           : inst.h
+  * @author         : toastoffee
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2024/7/16
+  ******************************************************************************
+  */
+
+
+
+#ifndef LUA5_3_DECOMPILER_INST_H
+#define LUA5_3_DECOMPILER_INST_H
+
+#include "../state/lua_value.hpp"
+#include "../api/lua_state.hpp"
+
+#include "instruction_types.hpp"
+#include "instruction_decode.hpp"
+
+typedef LuaState LuaVM;
+
+class Inst {
+
+    static void Move(Instruction i, LuaVM vm);
+
+    static void Jmp(Instruction i, LuaVM vm);
+
+};
+
+#endif //LUA5_3_DECOMPILER_INST_H
