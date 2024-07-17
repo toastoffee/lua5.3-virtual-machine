5b8a898561d28cfe6c5c4f9177a2223bad19a344
toastoffee <931391442@qq.com>
2024-07-17 19:09:11 +0800

    fix compile bugs

--- src/vm/inst.h
+++ src/vm/inst.h
@@ -47,4 +47,104 @@ class Inst {
     static void Bnot(Instruction i, LuaVM vm);
 };
 
+void Inst::Move(Instruction i, LuaVM vm) {
+    int a, b, _;
+    ABC(i, a, b, _);
+    a += 1;
+    b += 1;
+    vm.Copy(b, a);
+}
+
+void Inst::Jmp(Instruction i, LuaVM vm) {
+    int a, sbx;
+    AsBx(i, a, sbx);
+    vm.AddPC(sbx);
+
+    assert(a == 0 && "todo!");
+}
+
+
+void Inst::LoadNil(Instruction i, LuaVM vm) {
+    int a, b, _;
+    ABC(i, a, b, _);
+    a += 1;
+
+    vm.PushNil();
+    for (int j = a; j < a+b; ++j) {
+        vm.Copy(-1, j);
+    }
+    vm.Pop(1);
+}
+
+void Inst::LoadBool(Instruction i, LuaVM vm) {
+    int a, b, c;
+    ABC(i, a, b, c);
+    a += 1;
+
+    vm.PushBoolean(b != 0);
+    vm.Replace(a);
+    if(c != 0) {
+        vm.AddPC(1);
+    }
+}
+
+void Inst::LoadK(Instruction i, LuaVM vm) {
+    int a, bx;
+    ABx(i, a, bx);
+    a += 1;
+
+    vm.GetConst(bx);
+    vm.Replace(a);
+}
+
+void Inst::LoadKx(Instruction i, LuaVM vm) {
+    int a, _;
+    ABx(i, a, _);
+    a += 1;
+
+    int ax;
+    Ax(vm.Fetch(),ax);
+
+    vm.GetConst(ax);
+    vm.Replace(a);
+}
+
+
+void _binaryArith(Instruction i, LuaVM vm, ArithOp op) {
+    int a, b, c;
+    ABC(i, a, b, c);
+    a += 1;
+
+    vm.GetRK(b);
+    vm.GetRK(c);
+    vm.Arith(op);
+    vm.Replace(a);
+}
+
+void _unaryArith(Instruction i, LuaVM vm, ArithOp op) {
+    int a, b, _;
+    ABC(i, a, b, _);
+    a += 1;
+    b += 1;
+
+    vm.PushValue(b);
+    vm.Arith(op);
+    vm.Replace(a);
+}
+
+void Inst::Add(Instruction i, LuaVM vm) { _binaryArith(i, vm, LUA_OPADD); }
+void Inst::Sub(Instruction i, LuaVM vm) { _binaryArith(i, vm, LUA_OPSUB); }
+void Inst::Mul(Instruction i, LuaVM vm) { _binaryArith(i, vm, LUA_OPMUL); }
+void Inst::Mod(Instruction i, LuaVM vm) { _binaryArith(i, vm, LUA_OPMOD); }
+void Inst::Pow(Instruction i, LuaVM vm) { _binaryArith(i, vm, LUA_OPPOW); }
+void Inst::Div(Instruction i, LuaVM vm) { _binaryArith(i, vm, LUA_OPDIV); }
+void Inst::Idiv(Instruction i, LuaVM vm) { _binaryArith(i, vm, LUA_OPIDIV); }
+void Inst::Band(Instruction i, LuaVM vm) { _binaryArith(i, vm, LUA_OPBAND); }
+void Inst::Bor(Instruction i, LuaVM vm) { _binaryArith(i, vm, LUA_OPBOR); }
+void Inst::Bxor(Instruction i, LuaVM vm) { _binaryArith(i, vm, LUA_OPBXOR); }
+void Inst::Shl(Instruction i, LuaVM vm) { _binaryArith(i, vm, LUA_OPSHL); }
+void Inst::Shr(Instruction i, LuaVM vm) { _binaryArith(i, vm, LUA_OPSHR); }
+void Inst::Unm(Instruction i, LuaVM vm) { _binaryArith(i, vm, LUA_OPUNM); }
+void Inst::Bnot(Instruction i, LuaVM vm) { _binaryArith(i, vm, LUA_OPBNOT); }
+
 #endif //LUA5_3_DECOMPILER_INST_H
--- src/vm/inst_misc.cpp
+++ src/vm/inst_misc.cpp
@@ -10,18 +10,18 @@
 
 #include "inst.h"
 
-void Inst::Move(Instruction i, LuaVM vm) {
-    int a, b, _;
-    ABC(i, a, b, _);
-    a += 1;
-    b += 1;
-    vm.Copy(b, a);
-}
-
-void Inst::Jmp(Instruction i, LuaVM vm) {
-    int a, sbx;
-    AsBx(i, a, sbx);
-    vm.AddPC(sbx);
-
-    assert(a == 0 && "todo!");
-}
+//void Inst::Move(Instruction i, LuaVM vm) {
+//    int a, b, _;
+//    ABC(i, a, b, _);
+//    a += 1;
+//    b += 1;
+//    vm.Copy(b, a);
+//}
+//
+//void Inst::Jmp(Instruction i, LuaVM vm) {
+//    int a, sbx;
+//    AsBx(i, a, sbx);
+//    vm.AddPC(sbx);
+//
+//    assert(a == 0 && "todo!");
+//}
